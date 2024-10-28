5737d8a08afc2c492e8a79a7b0ba8e122dcc2c2a
anber-02 <newprogrammer02@gmail.com>
2024-10-28 08:51:31 -0600

    configurando el banner de la pwa

--- src/components/PwaPrompt.jsx
+++ src/components/PwaPrompt.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react'
 
 export const PwaPrompt = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null)
-  const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
     const handleBeforeInstallPrompt = (e) => {
-      console.log('Hola soy la funcion handleBeforeInstall')
       e.preventDefault()
       setDeferredPrompt(e)
       setIsVisible(true)
@@ -20,12 +19,11 @@ export const PwaPrompt = () => {
   }, [])
 
   const handleInstallClick = () => {
-    console.log('')
     if (deferredPrompt) {
       deferredPrompt.prompt()
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          setIsVisible(false) // Oculta el banner si se acepta
+          setIsVisible(false) // Oculta el banner solo si se acepta la instalación
         } else {
           console.log('Usuario rechazó la instalación de la PWA')
         }
@@ -35,14 +33,8 @@ export const PwaPrompt = () => {
 
   return (
     isVisible && (
-      <div className=" bg-blue-600 text-white p-4 mt-20 ">
+      <div className="bg-blue-600 text-white p-4 mt-20">
         <span>¡Instala nuestra app para una mejor experiencia!</span>
-        <button
-          className="ml-4 bg-blue-800 p-2 rounded"
-          onClick={() => setIsVisible(false)}
-        >
-          Cerrar
-        </button>
         <button
           className="ml-4 bg-blue-800 p-2 rounded"
           onClick={handleInstallClick}
@@ -53,3 +45,59 @@ export const PwaPrompt = () => {
     )
   )
 }
+
+// import React, { useEffect, useState } from 'react'
+
+// export const PwaPrompt = () => {
+//   const [deferredPrompt, setDeferredPrompt] = useState(null)
+//   const [isVisible, setIsVisible] = useState(true)
+
+//   useEffect(() => {
+//     const handleBeforeInstallPrompt = (e) => {
+//       console.log('Hola soy la funcion handleBeforeInstall')
+//       e.preventDefault()
+//       setDeferredPrompt(e)
+//       setIsVisible(true)
+//     }
+
+//     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+
+//     return () => {
+//       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+//     }
+//   }, [])
+
+//   const handleInstallClick = () => {
+//     console.log('')
+//     if (deferredPrompt) {
+//       deferredPrompt.prompt()
+//       deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//           setIsVisible(false) // Oculta el banner si se acepta
+//         } else {
+//           console.log('Usuario rechazó la instalación de la PWA')
+//         }
+//       })
+//     }
+//   }
+
+//   return (
+//     isVisible && (
+//       <div className=" bg-blue-600 text-white p-4 mt-20 ">
+//         <span>¡Instala nuestra app para una mejor experiencia!</span>
+//         <button
+//           className="ml-4 bg-blue-800 p-2 rounded"
+//           onClick={() => setIsVisible(false)}
+//         >
+//           Cerrar
+//         </button>
+//         <button
+//           className="ml-4 bg-blue-800 p-2 rounded"
+//           onClick={handleInstallClick}
+//         >
+//           Instalar
+//         </button>
+//       </div>
+//     )
+//   )
+// }
