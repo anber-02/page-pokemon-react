77c80654997dc747dab306e4ff03b61a13c19539
anber-02 <newprogrammer02@gmail.com>
2024-10-28 08:43:06 -0600

    cambiando el icon del pwa

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         background_color: '#f7f0eb',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'public/pikachu.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'public/pikachu.png',
             sizes: '512x512',
             type: 'image/png'
           }
